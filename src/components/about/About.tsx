f92b8e3ab6a09fd93849b5327951835f7fd5955e
Hoyame <zou.hoyame@gmail.com>
2020-05-08 03:24:56 +0200

    push

---
+++ src/components/about/About.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+import './About.scss'
+
+class About extends Component {
+  render() {
+    return (
+        <div>
+          <h2>About</h2>
+        </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+import './Navigation.scss'
+
+class Navigation extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Home</h2>
+        </div>
+    );
+  }
+}
+
+export default Navigation;
