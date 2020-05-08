948d824ca9f4fbc3675700328009b430dc5546e3
Hoyame <zou.hoyame@gmail.com>
2020-05-08 05:19:49 +0200

    cleaner

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -8,11 +8,11 @@ import './navigation.scss'
 class Navigation extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <ul className="navbar-nav mr-auto">
-          <li><Link to={'/'} className="nav-link"> About </Link></li>
-          <li><Link to={'/competences'} className="nav-link">Competences</Link></li>
-          <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+      <nav className="">
+        <ul className="">
+          <li><Link to={'/'} className=""> About </Link></li>
+          <li><Link to={'/competences'} className="">Competences</Link></li>
+          <li><Link to={'/contact'} className="">Contact</Link></li>
         </ul>
       </nav>
     );
