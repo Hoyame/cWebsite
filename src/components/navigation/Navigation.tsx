7c7ae396d693aa640b3c60007c17ce65663dc9b8
Hoyame <zou.hoyame@gmail.com>
2020-05-08 05:34:01 +0200

    commencement du style de la navbar

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -8,12 +8,21 @@ import './navigation.scss'
 class Navigation extends Component {
   render() {
     return (
-      <nav className="">
-        <ul className="">
-          <li><Link to={'/'} className=""> About </Link></li>
-          <li><Link to={'/competences'} className="">Competences</Link></li>
-          <li><Link to={'/contact'} className="">Contact</Link></li>
-        </ul>
+      <nav className = "nav">
+        <div className = "nav-container">
+
+          <div>
+            <span className = "nav-text">Hoyame Zouhari</span>
+          </div>
+
+          <div className = "nav-buttons">
+            <ul className = "">
+              <a><Link to={'/'} className=""> About </Link></a>
+              <a><Link to={'/competences'} className=""> Competences </Link></a>
+              <a><Link to={'/contact'} className=""> Contact </Link></a>
+            </ul>
+          </div>
+        </div>
       </nav>
     );
   }
