d14bb2c35533be4b659e5fd37bee6358e4b2e771
William Desgardin <w.desgardin@delssi-services.com>
2020-05-09 21:57:17 +0200

    Help on layout

--- src/components/pages/me/me.tsx
+++ src/components/pages/me/me.tsx
@@ -6,22 +6,19 @@ import './me.scss'
 class Me extends Component {
   render() {
     return (
-        <div>
-          <div className = "pages-body">
-
-            <div className = "me-h2-title">
-              <h2 className = "">Yo, c'est moi 🙃</h2>
+      <div className="pages-body">
+          <article className="presentation">
+            <div className="summary">
+              <h2 className="">Yo, c'est moi 🙃</h2>
               <p>fzefz</p>
             </div>
-
-           {/* <div className = "me-box">
+            <div className = "me-box">
               <img src="https://cdn.discordapp.com/avatars/643225867929124874/7b03c008c7de478d7acd707ab28ccfef.png?size=256" className = "me-box-avatar" alt="Hoyame"></img>
               <h3 className = "me-box-name">Hoyame</h3>
               <h6 className = "me-box-name2">@corazon2</h6>
             </div>
-    */}
-          </div>
-        </div>
+          </article>
+      </div>
     );
   }
 }
