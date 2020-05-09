a71d88f0ac74cce7fc3f2adcc407b9357914ea9d
Hoyame <zou.hoyame@gmail.com>
2020-05-09 18:16:06 +0200

    pl

--- src/components/pages/me/me.tsx
+++ src/components/pages/me/me.tsx
@@ -11,13 +11,15 @@ class Me extends Component {
 
             <div className = "me-h2-title">
               <h2 className = "">Yo, c'est moi 🙃</h2>
+              <p>fzefz</p>
             </div>
 
-            <div className = "me-box">
+           {/* <div className = "me-box">
               <img src="https://cdn.discordapp.com/avatars/643225867929124874/7b03c008c7de478d7acd707ab28ccfef.png?size=256" className = "me-box-avatar" alt="Hoyame"></img>
               <h3 className = "me-box-name">Hoyame</h3>
               <h6 className = "me-box-name2">@corazon2</h6>
             </div>
+    */}
           </div>
         </div>
     );
