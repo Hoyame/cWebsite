699066cd2941ae40315c3e3b55cac414cb70e9a2
Hoyame <zou.hoyame@gmail.com>
2020-05-11 18:14:36 +0200

    cmc page opensource

---
+++ src/components/pages/opensource/components/page.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+import './page.scss'
+import '../opensource.scss'
+
+interface IPageOpenSource {
+   title?: string,
+   img?: any,
+   description?: any,
+   new?: boolean
+}
+
+const PageOpenSourceProps : IPageOpenSource[] = [
+   { title: 'Article 1', img: "https://cdn.discordapp.com/avatars/643225867929124874/7b03c008c7de478d7acd707ab28ccfef.png?size=256", description: "Azertruyehugbureybguyerbguerbgurebguyebgb", new: true },
+   { title: 'Article 1', img: "https://cdn.discordapp.com/avatars/643225867929124874/7b03c008c7de478d7acd707ab28ccfef.png?size=256", description: "Azertruyehugbureybguyerbguerbgurebguyebgb", new: true },
+   { title: 'Article 1', img: "https://cdn.discordapp.com/avatars/643225867929124874/7b03c008c7de478d7acd707ab28ccfef.png?size=256", description: "Azertruyehugbureybguyerbguerbgurebguyebgb", new: true },
+]
+
+/////////////////////////////////////////////////////
+
+
+const IPageOpenSource: React.FC<IPageOpenSource> = (props: IPageOpenSource) => {  
+   return ( 
+      <div>
+         <a>
+            <div className = "page-opensource-container">
+               <img src = {props.img} className = "page-opensource-container-img"></img>
+               <p className = "page-opensource-container-text">{props.title}</p>
+               {props.description ? <p className = "page-opensource-container-description">{props.description}</p> : false}
+               {props.new ? <div className = "page-opensource-container-new">New</div> : false}
+            </div>
+         </a>
+      </div>
+   );
+};
+
+const getPageOpenSource = () => {
+   return PageOpenSourceProps.map((item, k) => {
+       return (
+           <IPageOpenSource {...item} />
+       )
+   })
+}
+
+class PageOpenSourceList extends Component {
+   render() {
+       return (
+            <div className="opensource-box">
+               {getPageOpenSource()}
+            </div>
+       );
+   }
+}
+
+export default PageOpenSourceList;
--- src/components/pages/opensource/opensource.tsx
+++ src/components/pages/opensource/opensource.tsx
@@ -2,12 +2,26 @@ import * as React from 'react';
 import { Component } from 'react';
 
 import './opensource.scss'
+import PageOpenSourceList from './components/page'
+
+let githubImg = "https://cdn.discordapp.com/avatars/643225867929124874/7b03c008c7de478d7acd707ab28ccfef.png?size=256"
+let githubName = "Hoyame Z."
+let githubLink = "ss"
+let githubID = "@corazon2"
 
 class OpenSource extends Component {
   render() {
     return (
         <div>
-          <h2>OpenSource</h2>
+          <h1 className = "opensource-head-title">OpenSource</h1>
+
+          <div className = "opensource-head-container">
+            <img src = {githubImg} className = "opensource-head-container-img"></img>
+            <p className = "opensource-head-container-text">{githubName}</p>
+            <p className = "opensource-head-container-link">{githubID}</p>
+            <p className = "opensource-head-container-id">{githubLink}</p>
+          </div>
+          <PageOpenSourceList />
         </div>
     );
   }
