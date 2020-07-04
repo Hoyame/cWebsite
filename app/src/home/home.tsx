e20d4cf541834b1fb955a369e72e76bd0f8bfb76
Hoyame <zou.hoyame@gmail.com>
2020-07-04 14:08:40 +0200

    feat: rework history

--- app/src/home/home.tsx
+++ app/src/home/home.tsx
@@ -27,21 +27,17 @@ const HomePage = () => {
           <div className = "articles">
             <h2 className = "summary-text">Mon "Histoire" ⏳</h2>
 
-            <p className = "summary-text-p">Bon je pense que vous savez comment je m'appele, dans ces paragraphes 
-            que j'ecris en ecoutant SCH je vais vous racconter ma vie, donc si vous etes pas interessées, pas la 
-            peine de lire, descendez :) , je suis née le 10/06/2004 en italie, c'est en italie que j'ai passée 10 
-            ans de ma vie, c'est la bas que j'ai trouvée ma passion, le pc, du coup je continuais, après quand je suis venue en france, je n'avais pas de pc, 
-            en 2016 ma mere ma acheter une ps3, 4 mois après ma mere est tombée malade, du coup j'etais au foyer pendant
-            un an sans tel ni ps3, la GALEREEEEEEE, ensuite après 7 mois d'attente sans la voir, elle est revenu soignée,
-            la j'ai eu la ps4 mais 2 mois après elle est retombée malade, du coup j'ai été placée en famille d'acceuil,
-            après j'ai vendu ma ps4 pour racheter un pc portable, c'est la que je me suis interessée au serveur gta rp, 
+            <p className = "summary-text-p">Bon je pense que vous savez comment je m'appelle, dans ces paragraphes 
+            que j'écris en écoutant SCH je vais vous raconter ma vie, donc si vous êtes pas intéressées, pas la 
+            peine de lire, descendez :) , je suis née le 10/06/2004 en Italie, c'est en Italie que j'ai passée 10 
+            ans de ma vie, en 2019 je me suis intéressée au serveur gta rp, 
             du coup j'ai lancer mon serveur bluevinity, qui après 8 mois, a été vendu par moi car on avais plus de joueurs
             a force de se faire ddos jour et nuit, sans solutions, j'en avais marre, ensuite 4 mois après j'ai voulu faire un 
-            framework pour le serveur fivem de mon ami Jibril, après 2 mois, j'ai abbandonnée car je ne voulais pas perdre de
-            temps pour un truc qui n'etais meme pas sur de marcher, un mois après j'ai vu le pPhone, j'ai aimée le concept,
-            c'est la que j'ai connu react et William qui m'a tout appris sur le dev web, car jusqu'a ce jour la, j'en avais 
+            framework pour le serveur fivem de mon ami Jibril, après 2 mois, j'ai abandonnée car je ne voulais pas perdre de
+            temps pour un truc qui n'étais même pas sur de marcher, un mois après j'ai vu le pPhone, j'ai aimée le concept,
+            c'est la que j'ai connu react et William qui m'a tout appris sur le dev web, car jusqu'à ce jour la, j'en avais 
             jamais fait, c'est depuis ce jour la que je fait du dev web, il y'a beaucoup de choses que je dois encore apprendre
-            mais je suis motivée :). Voila un bon gros resumée de mon histoire :D
+            mais je suis motivée :). Voila un bon gros résumée de mon histoire :smile:
             
             </p>
 
