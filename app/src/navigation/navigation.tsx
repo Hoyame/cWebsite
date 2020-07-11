6174d509d00abf9deec8814c64c1746213f77909
Hoyame <zou.hoyame@gmail.com>
2020-07-11 13:32:19 +0200

    feat: add github redirection

--- app/src/navigation/navigation.tsx
+++ app/src/navigation/navigation.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 
 import '../../styles/navigation/navigation.scss'
 
 
 const Navigation = () => {
+  const redirectGithub = () => {
+    return (
+      window.location.replace('https://github.com/hoyame')
+    );
+  }
+
   return (
     <nav className="nav">
       <Link to={'/'} className="nav-text">Hoyame Zouhari</Link>
       <ul className="nav-links">
         <Link to={'/about'} className="nav-button">About</Link>
-        <Link to={'/github'} className="nav-button">GitHub</Link>
+        <Link to={''} onClick={() => redirectGithub()} className="nav-button">GitHub</Link>
+        
         <Link to={'/contact'} className="nav-button">Contact</Link>
         <Link to={'/cv/scolaire'} className="nav-button">CV</Link>
       </ul>
