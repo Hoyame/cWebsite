fd810003e046e61db6f86c39377e0cd8136e8f6b
Hoyame <zou.hoyame@gmail.com>
2020-07-11 15:34:20 +0200

    tweak: remove warning message

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -14,7 +14,6 @@ const App = () => {
   return (
     <div className="color">
       <div className="container">
-        <div className="message-one">En développement</div>
         <Suspense fallback={loading()}>
           <HashRouter basename='/'>
             <Navigation />
